bb9db82215e9fb49be81c9d022c3492c2cdd2975
S-ATHIRA-2005 <athirasekhar18@gmail.com>
2026-07-28 18:40:06 +0530

    Update Variables.java

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -2,7 +2,16 @@ public class Student{
   public static void main(String[]args)
   {
     int age = 10;
+    String name ="Athira";
+    double CGPA = 8.35;
+    String Grade ="A";
+    boolean placed = false;
     System.out.println("age is :" +age) ;
+    System.out.println("name is :" +name) ;
+    System.out.println("CGPA is :" +CGPA) ;
+    System.out.println("Grade is :" +Grade) ;
+    System.out.println("placed is :" +placed) ;
+
   }
 }
     
